9cd4819a34ffce2531f9da0386525e9e844c9e42
Angel Biedma <tekeiro@gmail.com>
2017-01-21 01:27:23 +0100

    Initial commit. LibGdx project created

---
+++ desktop/src/com/keirobm/fwcards/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.keirobm.fwcards.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.keirobm.fwcards.FWCardsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FWCardsGame(), config);
+	}
+}
